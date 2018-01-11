ef272b3da26e1c7046f7b6f11e57018837b4628f
Azlan Cuttilan <azlan@bynd.com>
2018-01-11 17:28:24 +0000

    Client bundles
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

---
+++ server/webpack.client.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    // Tell Webpack the root file of our 
+    // server application
+    entry: './src/client/client.js',
+
+    // Tell Webpack where to put the output file
+    // that is generated
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+
+    // Tell Webpack to run Babel on every file it runs through
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        'react',
+                        'stage-0',
+                        ['env', { targets: { browsers: ['last 2 versions'] }}]
+                    ]
+                }
+            }
+        ]
+    }
+};
