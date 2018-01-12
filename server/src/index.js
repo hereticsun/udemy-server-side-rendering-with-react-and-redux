57e06dbfae59628013aad46de77544e049bcbb47
Azlan Cuttilan <azlan@bynd.com>
2018-01-12 09:40:32 +0000

    Ignoring files with Webpack
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import React from 'react';
-import { renderToString} from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 
 import Home from './client/components/Home';
 
