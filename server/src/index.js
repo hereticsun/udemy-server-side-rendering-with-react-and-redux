de812a11a2dc00770e6bb19c8513259fd6f1af6d
Azlan Cuttilan <azlan@bynd.com>
2018-01-12 15:04:52 +0000

    The matchRoutes function
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
 
+
+import Routes from './client/Routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
@@ -9,6 +12,8 @@ const app = express();
 app.use(express.static('public'));
 app.get('*', (req,res) => {
     const store = createStore();
+
+    console.log(matchRoutes(Routes, req.path));
 
     res.send(renderer(req, store));
 });
